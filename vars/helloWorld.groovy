8302b625c9051061e47ec26e9235c66b790b7761
Trylisin <50042805+Trylisin@users.noreply.github.com>
2022-12-07 17:42:01 +0700

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,30 @@
 def call(Map config = [:]) {
-  echo "hello ${config.name}, nice to meet you ${config.day}. Just for testing ${config.test}"
+//   echo "hello ${config.name}, nice to meet you ${config.day}. Just for testing ${config.test}"
+  
+  def app
+  stage "Pull image"
+  if ("!${config.ImageNmme}") {ex(config.ImageNmme)}
+  app = docker.image("${config.ImageName}")
+  app.pull()
+  sh "docker images"
+
+  // stage "Run Image"
+  // sh "docker run -d -p 8888:80 prakhar1989/static-site"
+  // app.run("-p 8888:80")
+
+//   stage "Push Image"
+//   docker.withRegistry("", "${CredentialID}") {
+      // app.push('latest')
+      // sh "docker tag prakhar1989/static-site trylisin/testing"
+//       if ("${Repository}" != "OK") {ex("Repository")}
+//       sh "docker tag $app.id ${Repository}/${TagName}"
+//       sh "docker push ${Repository}/${TagName}"
+//       sh "docker logout"
+//   }
+}
+
+
+def ex(param){
+    currentBuild.result = 'ABORTED'
+    error('BAD PARAM: ' + param)
 }
