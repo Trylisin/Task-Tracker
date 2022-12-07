dbae3d1001dac3da6a7a90887d10a2a1ae6a76e5
Trylisin <50042805+Trylisin@users.noreply.github.com>
2022-12-07 22:27:01 +0700

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -2,25 +2,34 @@ def call(Map config = [:]) {
 //   echo "hello ${config.name}, nice to meet you ${config.day}. Just for testing ${config.test}"
   
   def app
+  def ImageNmme = config.ImageNmme
+  def CredentialID = config.CredentialID
+  def Repository = config.Repository
+  def TagName = config.TagName
+  
+  if ("!${ImageNmme}") {ex(ImageNmme)}
+  if ("!${CredentialID}") {ex(CredentialID)}
+  if ("!${Repository}") {ex(Repository)}
+  if ("!${TagName}") {ex(TagName)}
+  
   stage "Pull image"
-  if ("!${config.ImageNmme}") {ex(config.ImageNmme)}
-  app = docker.image("${config.ImageName}")
+  app = docker.image("${ImageName}")
   app.pull()
   sh "docker images"
 
-  // stage "Run Image"
-  // sh "docker run -d -p 8888:80 prakhar1989/static-site"
-  // app.run("-p 8888:80")
+//   stage "Run Image"
+//   sh "docker run -d -p 8888:80 prakhar1989/static-site"
+//   app.run("-p 8888:80")
 
-//   stage "Push Image"
-//   docker.withRegistry("", "${CredentialID}") {
-      // app.push('latest')
-      // sh "docker tag prakhar1989/static-site trylisin/testing"
+  stage "Push Image"
+  docker.withRegistry("", "${CredentialID}") {
+//       app.push('latest')
+//       sh "docker tag prakhar1989/static-site trylisin/testing"
 //       if ("${Repository}" != "OK") {ex("Repository")}
-//       sh "docker tag $app.id ${Repository}/${TagName}"
-//       sh "docker push ${Repository}/${TagName}"
-//       sh "docker logout"
-//   }
+      sh "docker tag $app.id ${Repository}/${TagName}"
+      sh "docker push ${Repository}/${TagName}"
+      sh "docker logout"
+  }
 }
 
 
